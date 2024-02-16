2ba2cdc779a75f216aa86bdf14335cf54a1a8823
Christian-M17 <christianotteromarques@gmail.com>
2024-02-16 03:46:40 -0300

    Upgrade

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
+var nomearquivo = 'times.csv'
+const planilha = fs.promises.readFile(nomearquivo);
 
-const planilha = fs.promises.readFile('times.csv');
-
-planilha.then((arquivo) => {
-    console.log(arquivo.toString('utf8'))
+planilha
+.then((arquivo) => {
+    console.log(arquivo.toString('utf8'))})
+.catch((error) => {
+    console.log ('Problema! ', error)
 })
+.finally(() => console.log("Obrigado por usar o Readfiles, desenvolvido por Christian-M17")) 
